1053fbd400d1cf11b47f1dbe986a9c1699a3bbe7
= <=>
2024-08-24 10:13:29 +0200

    Implemented parent class import

--- acquire/deischannel.py
+++ acquire/deischannel.py
@@ -1,6 +1,28 @@
 from pybiologic import Channel
 
 class DEISchannel(Channel):
-    def __init__(self, picoscope, awg):
+    def __init__(self, 
+                 bio_device : BiologicDevice, 
+                 channel_num : int, 
+                 saving_dir : str,
+                 channel_options : namedtuple,
+                 do_live_plot : bool = True, # ? Deside which naming convention to use for booleans
+                 do_record_Ece : bool = False,
+                 do_record_analog_in1 : bool = False,
+                 do_record_analog_in2 : bool = False,
+                 do_print_values : bool  = False,
+                 picoscope = None, 
+                 awg = None):
+
+        super().__init__(bio_device, 
+                         channel_num,
+                         saving_dir,
+                         channel_options,
+                         do_live_plot,
+                         do_record_Ece,
+                         do_record_analog_in1,
+                         do_record_analog_in2,
+                         )
+                         
         self.pico = piscoscope,
         self.awg = awg
