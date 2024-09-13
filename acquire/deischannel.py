e0d81eaf43d84e04354d0ab00ad3e6df8397d7d9
Federico Scarpioni <federicoscarpioni@gmail.com>
2024-09-13 15:51:04 +0200

    Corrected, it works now
    Exept for the saving of the picoscope

--- acquire/deischannel.py
+++ acquire/deischannel.py
@@ -1,22 +1,29 @@
+from pyeclab.channel import Channel
+from pypicostreaming.pypicostreaming.series5000.series5000 import Picoscope5000a    
+
+
 class DEISchannel():
     def __init__(self, 
                  potentiostat_channel : Channel,
-                 picoscope,
-                 trueform_awg=None):
+                 picoscope : Picoscope5000a,
+                 trueform_awg = None):
         self.pot = potentiostat_channel
-        self.pico = picoscope,
-        self.pot.callbacks.append(self.save_pico_intermediate())
-        if trueform_awg != None:
-            self.awg = trueform_awg
-            # !!! Add the callback to change wave in the awg
+        self.pico = picoscope
 
 
     def run(self):
-        self.awg.turn_on()
-        self.pot.start()
         self.pico.run_streaming_non_blocking()
+        self.pot.start()
+        self.pot.callbacks.append(self.save_pico_intermediate)
+
 
     def save_pico_intermediate(self):
         self.pico.convert_all_channels()
-        self.pico.save_signals(f'cycle{self.pot.data_info.loop - 1}/sequence_{self.pot.current_tech_index}_tech_{self.pot.current_tech_id}')
+        self.pico.save_signals(f'/cycle_{self.pot.current_loop-1}/sequence_{self.pot.current_tech_index}')
         self.pico.reset_buffer()
+
+    
+def stop(self):
+        self.pot.stop()
+        self.pico.stop()
+        self.save_pico_intermediate()
