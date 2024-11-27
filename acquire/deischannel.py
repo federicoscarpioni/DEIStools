b2b6ce89f7ceee121b541b0c7df273d856d6dff0
= <=>
2024-11-27 10:47:26 +0100

    Fixed technique and cycle counters
    adding a class that keep track of the changes. has to be tested on the device

--- acquire/deischannel.py
+++ acquire/deischannel.py
@@ -26,5 +26,24 @@ class DEISchannel():
         self.pico.save_intermediate_signals(f'/cycle_{self.pot.current_loop}/sequence_{self.pot.current_tech_index}')
 
 
-def stop(self):
-        self.pot.stop()
+    def stop(self):
+            self.pot.stop()
+
+
+class SequenceCounter:
+    '''
+    This class counts the current techniques index and number of loops of the technique in the potentiostat. It is
+    created because it is not possible the get the correct number of loops and technique index from the Channel class
+    that represents the instrument due to the concurrency of the processes (multi-threading).
+    '''
+    def __init__(self, techniques_in_sequence):
+        self.loop = 0
+        self.techniques_in_sequence = techniques_in_sequence
+        self.technique_index = 0
+
+    def update(self):
+        self.technique_index = + 1
+        if self.technique_index == self.techniques_in_sequence:
+            self.loop =+ 1
+            self.technique_index = 0
+
