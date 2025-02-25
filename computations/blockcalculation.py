151121d760c79dd0197d3639f7c2f2cc75721034
= <=>
2025-02-25 15:42:19 +0100

    Create blockcalculation.py

---
+++ computations/blockcalculation.py
@@ -0,0 +1,7 @@
+class BlockCalculation:
+    def __init__(self, data, frequencies, ):
+        self.data = data
+        self.block_data = block.block_data
+        self.block_data['block'] = block
+
+    def calculate
