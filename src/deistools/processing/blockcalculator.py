abaf0db25621a6921327edcf5da38fe8c42882df
Federico Scarpioni <federicoscarpioni@gmail.com>
2025-04-07 17:08:17 +0200

    Corrected implementation while testing

--- src/deistools/processing/blockcalculator.py
+++ src/deistools/processing/blockcalculator.py
@@ -3,10 +3,10 @@ from dataclasses import dataclass, field
 import numpy as np
 from numpy.fft import ifft, ifftshift
 from npbuffer import NumpyCircularBuffer
-from mfa import MultiFrequencyAnalysis
+from deistools.processing.mfa import MultiFrequencyAnalysis
 
 
-@ dataclass
+@dataclass
 class BlockCalculator:
     input_size : int
     sampling_time : float
@@ -14,15 +14,16 @@ class BlockCalculator:
     lp_filter : np.array
     ds_factor : int
     buffer_size : int
-    impedance : np.array = field(default = np.zeroes((high_z_calculator.frequencies, input_size//ds_factor), dype = np.complex64))
     impedance_index: int = field(default= 0)
-    voltage_ds: NumpyCircularBuffer = field(default=NumpyCircularBuffer(buffer_size, np.float32))
-    current_ds: NumpyCircularBuffer = field(default=NumpyCircularBuffer(buffer_size, np.float32))
-    saving_dir : str
-    finished_tech : bool = field(default=False)
+    impedance : np.array = field(init=False)
+    voltage_ds: NumpyCircularBuffer = field(init=False)
+    current_ds: NumpyCircularBuffer = field(init=False)
     
     def __post_init__(self):
         self.high_z_calculator.compute_freq_axis()
+        self.voltage_ds = NumpyCircularBuffer(self.buffer_size, np.float32)
+        self.current_ds = NumpyCircularBuffer(self.buffer_size, np.float32)
+        self.reset_impedance_memory()
 
     def calculate(self, data_voltage, data_current):
         self.high_z_calculator.voltage = data_voltage
@@ -32,24 +33,24 @@ class BlockCalculator:
         self.impedance[:,self.impedance_index] = high_z
         self.impedance_index += 1
         # Decimate the signals
-        self.voltage_filt = self.input_size * ifft(ifftshift(self.high_z_calculator.ft_voltage * self.fd_filter)).real
-        self.current_filt = self.input_size * ifft(ifftshift(self.high_z_calculator.ft_current * self.fd_filter)).real
+        self.voltage_filt = self.input_size * ifft(ifftshift(self.high_z_calculator.ft_voltage * self.lp_filter)).real
+        self.current_filt = self.input_size * ifft(ifftshift(self.high_z_calculator.ft_current * self.lp_filter)).real
         self.voltage_ds.push(self.voltage_filt[::self.ds_factor])
         self.current_ds.push(self.current_filt[::self.ds_factor])
-        if self.finished_tech:
-            self._save_results()
-            self._reset_impedance_memory()
-    
-    def finish_technique(self):
-        self.finished_tech = True
+        print('Calculation completed!')
 
-    def _save_results(self):
-        np.save(self.saving_dir+'/impedance.npy', self.impedance[:,0:self.impedance_index])
-        np.save(self.saving_dir+'/voltage.npy', self.voltage_ds.empty())
-        np.save(self.saving_dir+'/current.npy', self.current_ds.empty())
+    def save_results(self, directory):
+        np.save(directory+'/impedance.npy', self.impedance[:,0:self.impedance_index])
+        self.reset_impedance_memory()
+        np.save(directory+'/voltage.npy', self.voltage_ds.empty())
+        np.save(directory+'/current.npy', self.current_ds.empty())
+        print('Data saved for the last technique!')
 
-    def _reset_impedance_memory(self):
-        self.impedance = np.zeroes((self.multisine_frequencies, self.voltage.size//self.ds_factor))
+    def reset_impedance_memory(self):
+        self.impedance = np.zeros(
+            (self.high_z_calculator.frequencies.size, self.buffer_size), # This is too much allocation! 
+            dtype = np.complex64,
+            ) 
         self.impedance_index  = 0
     
     
