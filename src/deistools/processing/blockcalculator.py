3050cfbe0f528819a959f175c2299d69713a21fd
Federico Scarpioni <federicoscarpioni@gmail.com>
2025-07-07 09:18:49 +0200

    MOved software limit check from pico to blockcalculator

--- src/deistools/processing/blockcalculator.py
+++ src/deistools/processing/blockcalculator.py
@@ -2,10 +2,25 @@
 from dataclasses import dataclass, field
 import numpy as np
 from numpy.fft import ifft, ifftshift
+
 from npbuffer import NumpyCircularBuffer
+
 from deistools.processing import MultiFrequencyAnalysis, FermiDiracFilter
 from deistools.processing import detrending
 
+from pyeclab import Channel
+
+
+@dataclass
+class ConditionAverageScope:
+    """
+    Quantity should be "current" or "voltage". Operator instead can only be ">" 
+    or "<".
+    """
+    technique_index : int
+    quantity : str
+    operator : str
+    threshold : float
 
 @dataclass
 class BlockCalculator:
@@ -15,6 +30,8 @@ class BlockCalculator:
     lp_filter : FermiDiracFilter
     ds_factor : int
     buffer_size : int
+    potentiostat : Channel
+    conditions: list[ConditionAverageScope] = field(default_factory=list)
     impedance_index: int = field(default= 0)
     impedance : np.array = field(init=False)
     voltage_ds: NumpyCircularBuffer = field(init=False)
@@ -27,8 +44,11 @@ class BlockCalculator:
         self.reset_impedance_memory()
 
     def calculate(self, data_voltage, data_current):
-        self.high_z_calculator.voltage, coordinates_voltage = detrending.remove_baseline(data_voltage, self.sampling_time)
-        self.high_z_calculator.current, coordinates_current = detrending.remove_baseline(data_current, self.sampling_time)
+        # self.high_z_calculator.voltage, coordinates_voltage = detrending.remove_baseline(data_voltage, self.sampling_time)
+        # self.high_z_calculator.current, coordinates_current = detrending.remove_baseline(data_current, self.sampling_time)
+        self.high_z_calculator.voltage = data_voltage
+        self.high_z_calculator.current = data_current
+
         # Compute impedance of the high frequency band
         self.high_z_calculator.compute_fft()
         if self.high_z_calculator.freq_indexes == None:
@@ -41,10 +61,13 @@ class BlockCalculator:
         # Decimate the signals
         self.voltage_filt = self.input_size * ifft(ifftshift(self.high_z_calculator.ft_voltage * self.lp_filter.values)).real
         self.current_filt = self.input_size * ifft(ifftshift(self.high_z_calculator.ft_current * self.lp_filter.values)).real
-        self.voltage_filt = detrending.redo_baseline(self.voltage_filt, coordinates_voltage)
-        self.current_filt = detrending.redo_baseline(self.current_filt, coordinates_current)
+        # self.voltage_filt = detrending.redo_baseline(self.voltage_filt, coordinates_voltage)
+        # self.current_filt = detrending.redo_baseline(self.current_filt, coordinates_current)
         self.voltage_ds.push(self.voltage_filt[::self.ds_factor])
         self.current_ds.push(self.current_filt[::self.ds_factor])
+        if self.check_software_limits():
+            print("Software limit scope met!")
+            self.potentiostat.end_technique()
 
     def save_results(self, directory):
         np.save(directory+'/impedance.npy', self.impedance[:,0:self.impedance_index])
@@ -59,3 +82,25 @@ class BlockCalculator:
             dtype = np.complex64,
             ) 
         self.impedance_index  = 0
+    
+    def check_software_limits(self):
+        """
+        Check if a certain averege condition (< or > of a treshold value) is met for a
+        value of the sampled data during the window of the online computation.
+        """
+        mfa = self.high_z_calculator
+        for condition in self.conditions:
+            if self.potentiostat.data_info.TechniqueIndex == condition.technique_index:
+                if condition.quantity == 'voltage':
+                    value_avarage = abs(mfa.ft_voltage[mfa.index_f0])
+                    print(f'Avarage voltage is {value_avarage} V')
+                if condition.quantity == 'current':
+                    value_avarage = abs(mfa.ft_current[mfa.index_f0])
+                    print(f'Avarage current is {value_avarage} A')
+                if condition.operator == ">" and value_avarage >= condition.threshold:
+                    print(f'{condition.quantity} > {condition.threshold}')
+                    return True
+                elif condition.operator == "<" and value_avarage <= condition.threshold:
+                    print(f'{condition.quantity} < {condition.threshold}')
+                    return True
+        return False
