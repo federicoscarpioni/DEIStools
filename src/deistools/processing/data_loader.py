8a1e17157c42a0e5dbddd98a3fef1794226fe4d8
Federico Scarpioni <federicoscarpioni@gmail.com>
2025-07-28 11:22:57 +0200

    Added pyeclab results loader

--- src/deistools/processing/data_loader.py
+++ src/deistools/processing/data_loader.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 from tkinter import Tk, filedialog
+import pandas as pd
 
 def load_impedance_set(text : str = 'Select npy impedance file'):
     # Open dialog window to select pkl file containing data
@@ -25,6 +26,13 @@ def load_voltage_current(text : str = "Select directory with voltage and current
     voltage = np.load(directory + '/voltage.npy')
     current = np.load(directory + '/current.npy')
     return voltage, current, directory
+
+def load_pyeclab_data(text: str = "Select csv file"):
+    root = Tk()
+    root.withdraw()
+    root.attributes('-topmost', True)
+    file_path = filedialog.askopenfilename(title = text)
+    return pd.read_csv(file_path, sep = '\t')
 
 if __name__ == '__main__':
     load_impedance_set()
