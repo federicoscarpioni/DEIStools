e2762dbe981e096a0506d8be6ab51e4df5156be5
= <=>
2025-12-20 15:50:45 +0100

    Created loader function

--- src/deistools/processing/data_loader.py
+++ src/deistools/processing/data_loader.py
@@ -1,5 +1,5 @@
 import numpy as np
-import os
+import os, json
 from tkinter import Tk, filedialog
 import pandas as pd
 
@@ -14,6 +14,22 @@ def load_impedance_set(text : str = 'Select npy impedance file'):
     # Import impedance data
     Z = np.load(filename)
     return Z, directory
+
+def load_impedance_set_with_error(text : str = 'Select npy impedance file'):
+    # Open dialog window to select pkl file containing data
+    root = Tk() # pointing root to Tk() to use it as Tk() in program.
+    root.withdraw() # Hides small tkinter window.
+    root.attributes('-topmost', True) # Opened windows will be active. above all windows despite of selection.
+    # Returns file path as str
+    filename = filedialog.askopenfilename(title = text)
+    directory = os.path.dirname(filename)
+    # Import impedance data
+    Z = np.load(filename)
+    Zerr = np.load(directory+"/error_low_freq.npy")
+    metadata_dir = os.path.dirname(os.path.dirname(directory))
+    with open(metadata_dir + "/metadata_deis_exp.json", 'r') as file:  
+        frequencies = json.load(file)["Frequencies remaining (Hz)"]
+    return Z, Zerr, frequencies, directory
 
 def load_voltage_current(text : str = "Select directory with voltage and current npy files"):
     # Open dialog window to select pkl file containing data
